a04cc7c349777e03358de8c76b655a2354a768d2
SpoonFll <baker.jackson@rocketmail.com>
2022-09-23 14:50:47 -0400

    memory update fixes

--- src/impl/kernel/src/main.rs
+++ src/impl/kernel/src/main.rs
@@ -19,22 +19,21 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     init();
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let l4_table = unsafe {active_level_4_table(phys_mem_offset)};
-
     for(i,entry) in l4_table.iter().enumerate(){
-        if !entry.is_unused()
-        {
+        if !entry.is_unused() {
             println!("L4 Entry {}: {:?}",i,entry);
         }
     }
 
 
-    let addresses = [0xb8000,0x201008,0x0100_0020_1a10,boot_info.physical_memory_offset,];
+    let addresses = [0xb8000,0xde1000,0x0100_0020_1a10,boot_info.physical_memory_offset,];
     for &address in &addresses{
         let virt = VirtAddr::new(address);
         let phys = unsafe {translate_addr(virt, phys_mem_offset)};
         println!("{:?} -> {:?}",virt,phys);
     }
     println!("Great SUCCESS!");
+    println!("{}",boot_info.physical_memory_offset);
     hlt_loop();
 }
 fn init()
