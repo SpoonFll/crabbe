1c60dc591c80645dc51c71d16d1aa7ff901c8038
SpoonFll <baker.jackson@rocketmail.com>
2023-10-14 16:38:22 -0500

    working shell implemented

--- kernel/src/shell.rs
+++ kernel/src/shell.rs
@@ -7,9 +7,14 @@ use crate::{print, println};
 
 pub fn shell() {
     print!("MR. USERMAN $> ");
-    gets();
+    let mut command = String::new();
+    while command != "exit" {
+        command = gets();
+        print!("\n{}", command);
+    }
+    println!("exiting shell now goodbye!");
 }
-pub fn gets() {
+pub fn gets() -> String {
     activate_keyboard();
     let mut ioHandler = unsafe { io::STDIO.load(Ordering::SeqCst).as_ref() }.unwrap(); //flags implemented
     while ioHandler.get_byte(ioHandler.get_position()) != b'\n' {}
@@ -17,11 +22,19 @@ pub fn gets() {
     let buffer = str::from_utf8(&bufRaw).expect("could not be decoded");
     let end = buffer.find("\n").unwrap();
     let trimmed_buffer: String = buffer.chars().take(end).collect();
-    println!("{}", trimmed_buffer);
+    gets_exit_sequence();
+    trimmed_buffer
 }
 fn activate_keyboard() {
     let mut ioHandler = unsafe { io::STDIO.load(Ordering::SeqCst).as_mut() }.unwrap(); //flags implemented
     ioHandler.enable_in();
     ioHandler.enable_out();
     *io::KEYBOARD_ACTIVE.try_write().unwrap() = true;
+}
+fn gets_exit_sequence() {
+    let mut ioHandler = unsafe { io::STDIO.load(Ordering::SeqCst).as_mut() }.unwrap(); //flags implemented
+    ioHandler.disable_in();
+    ioHandler.disable_out();
+    *io::KEYBOARD_ACTIVE.try_write().unwrap() = false;
+    ioHandler.flush_buffer();
 }
